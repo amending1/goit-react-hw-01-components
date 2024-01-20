e1dd65b597619d1b084a9e8b1bd7fbe97dd63413
amending1 <joasia.sowiak@gmail.com>
2024-01-20 19:02:28 +0100

    adjustments

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,25 +2,34 @@ import css from './friendlist.module.css';
 
 import PropTypes from 'prop-types';
 
-function FriendListItem ({avatar, name, isOnline}) {
-    const variant = isOnline ? 'online' : 'offline';
-    const getBgColor = (variant) => { 
-        switch (variant) {
-            case 'online':
-                return "green";
-            case 'offline':
-                return 'red';
-                default:
-                    return 'white';
-        }
-    };
-    return (
-        <li className={css.item}>
-        <span className={css.status} style={{backgroundColor: getBgColor(variant) }}></span>
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-        <p className={css.name} >{name}</p>
-      </li>  
-    )
+function FriendListItem({ avatar, name, isOnline }) {
+  const variant = isOnline ? 'online' : 'offline';
+  const getBgColor = variant => {
+    switch (variant) {
+      case 'online':
+        return 'green';
+      case 'offline':
+        return 'red';
+      default:
+        return 'white';
+    }
+  };
+  return (
+    <li className={css.item}>
+      <span
+        className={css.status}
+        style={{ backgroundColor: getBgColor(variant) }}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
 
 export default FriendListItem;
